38744f72ed761de4ea00be83dce84c2e55a77ba9
YuriiPohorilets <pohorilets.y@gmail.com>
2022-07-22 17:27:29 +0300

    add mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,22 @@
+const menuBttns = document.querySelectorAll("[data-menu-button]");
+const mobileMenuRef = document.querySelector("[data-menu]");
+// const menuOverlay = document.querySelector(".menu-overlay");
+const menuNav = document.querySelector(".nav__list--group");
+
+menuBttns.forEach((el) => el.addEventListener("click", toggleMenuVisibility));
+// menuOverlay.addEventListener("click", toggleMenuVisibility);
+menuNav.addEventListener("click", toggleMenuVisibility);
+
+function toggleMenuVisibility() {
+  const expanded = document.body.classList.contains("menu-is-open");
+
+  menuBttns.forEach((el) => el.setAttribute("aria-expanded", !expanded));
+  document.body.classList.toggle("menu-is-open");
+  expanded
+    ? document.body.removeEventListener("keydown", onKeyDown)
+    : document.body.addEventListener("keydown", onKeyDown);
+}
+
+function onKeyDown(event) {
+  event.code === "Escape" ? toggleMenuVisibility() : null;
+}
